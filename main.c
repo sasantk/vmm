bd5f90dfb2eb3979938d7669cc7f0c6d783899b3
Sasan torabkheslat <sassan.torabkheslat@adjust.com>
2026-09-15 17:50:27 +0200

    add mmap and allocate mem for vm
    Signed-off-by: Sasan torabkheslat <sassan.torabkheslat@adjust.com>

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
 // #include <asm/kvm.h>
 #include <fcntl.h>
 #include <linux/kvm.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/ioctl.h>
+#include <sys/mman.h>
 #include <unistd.h>
 int main(void) {
 
@@ -29,8 +31,27 @@ int main(void) {
     return EXIT_FAILURE;
   }
   printf("%d\n", vm_fd);
+  void *addr = mmap(NULL, 32 * 1024, PROT_READ | PROT_WRITE,
+                    MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+  if (addr == MAP_FAILED) {
+    perror("err happens on mmaping");
+    return EXIT_FAILURE;
+  }
+
+  struct kvm_userspace_memory_region mem =
+      (struct kvm_userspace_memory_region){.slot = 0,
+                                           .guest_phys_addr = 0x0,
+                                           .memory_size = 32 * 1024,
+                                           .userspace_addr = (uintptr_t)addr};
+
+  int register_ret = ioctl(vm_fd, KVM_SET_USER_MEMORY_REGION, &mem);
+  if (register_ret < 0) {
+    perror("err happens on mem_fd");
+    return EXIT_FAILURE;
+  }
 
   close(vm_fd);
+  munmap(addr, 32 * 1024);
   close(kvm_fd);
   return EXIT_SUCCESS;
 }
